5fb23736ca74cfbd4cafadab9ff72cdf4a9167cc
FranciscoCardita <franciscocardita@ua.pt>
2024-03-30 16:51:40 +0000

    Refactor models

--- models/notification.py
+++ models/notification.py
@@ -1,9 +1,19 @@
 from pydantic import BaseModel
+from typing_extensions import TypedDict
+
+class NotificationContent(TypedDict):
+    subject: str
+    body: str
+
+class Preferences(TypedDict):
+    smsEnabled: bool
+    emailEnabled: bool
+    pushEnabled: bool
 
 class NotificationRequest(BaseModel):
     username: str
     email: str
     phoneNumber: str
     systemName: str
-    notificationContent: dict
-    preferences: dict
+    notificationContent: NotificationContent
+    preferences: Preferences
