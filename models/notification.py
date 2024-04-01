aec7137655374739377dbc93ca632bdf0b68f471
FranciscoCardita <franciscocardita@ua.pt>
2024-04-01 18:32:35 +0100

    Refactor models

--- models/notification.py
+++ models/notification.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
 from typing_extensions import TypedDict
 
 class NotificationContent(TypedDict):
@@ -12,8 +13,8 @@ class Preferences(TypedDict):
 
 class NotificationRequest(BaseModel):
     username: str
-    email: str
-    phoneNumber: str
+    email: EmailStr
+    phoneNumber: PhoneNumber
     systemName: str
     notificationContent: NotificationContent
     preferences: Preferences
