001dd238966a01ab285b8b2d49155e098d016eb4
FranciscoCardita <franciscocardita@ua.pt>
2024-04-01 18:45:29 +0100

    Add email and sms functionality

--- api/endpoints/notifications.py
+++ api/endpoints/notifications.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
 from models.notification import NotificationRequest
+from utils.notification_utils import send_sms, send_email
 
 router = APIRouter()
 
 @router.post("/notifications")
 async def send_notification(notification: NotificationRequest):
-    return {"message": "Notification sent successfully"}
+    if notification.preferences["smsEnabled"]:
+        send_sms(notification.phoneNumber, notification.notificationContent["body"])
+    
+    if notification.preferences["emailEnabled"]:
+        send_email(notification.email, notification.notificationContent["subject"], notification.notificationContent["body"])
+
+    if notification.preferences["pushEnabled"]:
+        pass
+    
+    return {"message": "Notification(s) sent successfully"}
